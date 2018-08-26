e4253a9a4fe519289a675f0bdfc9fc42ba33443f
Tim Lukens <tblukens87@gmail.com>
2018-08-26 11:40:14 -0500

    Small changes to index. Pseudo Coded KIND OF...

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+
+// Create object to hold game
+
+// setup variables for characters and health, attack etc...
+
+// list characters on html
+
+// when a character is selected move selected character to "#your-character"
+// also move enemies available to "#available-enemies"
+
+// when an enemy is selected move selection to "#defender"
+
+// when #attack button is clicked factor damage and change variables accordingly
+// NOTE: chosen characters damage goes up by base amount each attack
+// NOTE: enemies damage is constant
+// show damage in #flavor-text
+
+// if defender wins - game over
+// else continue with next enemy
+
+// when defender loses remove them
+
+// show that the enemy is defeated in #flavor-text and to choose another enemy
+
+// when all enemies are defeated show that the player has won and game is over
+// then show restart button in #flavor-text
